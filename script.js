2e79e9e0408527df6c01f5eedc5e7bfa2ab07043
Itz-kavish <91275857+Itz-kavish@users.noreply.github.com>
2022-01-15 00:04:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+//setting all the variables to get element from the html file
+let counterJS = 0;
+const decrease = document.getElementsByClassName("increase")[0];
+const reset = document.getElementsByClassName("reset")[0];
+const increase = document.getElementsByClassName("decrease")[0];
+const counter = document.getElementsByClassName("number")[0];
+const btns = document.querySelectorAll(".counterButtons");
+
+decrease.onclick = function(){
+    counter.innerHTML--;
+    counterJS--;
+    if (counterJS < 0){
+        counter.style.color = 'red';}
+    if (counterJS == 0){
+        counter.style.color = 'black';
+    }
+}
+reset.onclick = function(){
+    counter.innerHTML = 0;
+    counterJS = 0;
+    if (counterJS == 0){
+        counter.style.color = 'black';}
+}
+increase.onclick = function(){
+    counter.innerHTML++;
+    counterJS++;
+    if (counterJS > 0){
+        counter.style.color = 'green';
+    }
+    if (counterJS == 0){
+        counter.style.color = 'black';
+    }
+}

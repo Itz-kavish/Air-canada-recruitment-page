ad3889742946f9bfdad4cc042d3eca363f790a79
Itz-kavish <kavisharora159@gmail.com>
2022-01-21 14:31:49 -0800

    Toggle button

--- hex.js
+++ hex.js
@@ -1,18 +1,40 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
 const button = document.getElementById("themeBtn");
+const toggle = document.getElementsByClassName("fa-toggle-on")[0];
+const toggleDiv = document.querySelectorAll("toggleMode");
 const color = document.getElementsByClassName("color")[0];
+const root = document.querySelector(":root");
+const toggleOff = document.getElementsByClassName("fa-toggle-off")[0];
+
+toggle.addEventListener("click", function () {
+  root.style.setProperty("--grey", "#2D4263");
+  root.style.setProperty("--black", "#ECDBBA");
+  root.style.setProperty("--blue", "#191919");
+  root.style.setProperty("--light-red", "#ECDBBA");
+  root.style.setProperty("--red", "#2D4263");
+  root.style.setProperty("--off-white", "#C84B31");
+  root.style.setProperty("--teal", "#C84B31");
+  toggle.style.display = "none";
+  toggleOff.style.display = "block";
+});
+
+toggleOff.addEventListener("click", function () {
+  root.style.setProperty("--blue", "#e1fffa");
+  toggle.style.display = "block";
+  toggleOff.style.display = "none";
+});
 
 function generateRandomNum() {
   return Math.floor(Math.random() * hex.length);
 }
 
-document.addEventListener("click", function () {
+button.addEventListener("click", function () {
   let hexColor = "#";
+  let hexColor1 = "#";
   for (i = 0; i < 6; i++) {
     hexColor += hex[generateRandomNum()];
   }
   console.log(hexColor);
   document.body.style.backgroundColor = hexColor;
-  color.innerHTML = hexColor;
 });
